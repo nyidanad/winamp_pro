19b2e389fc300372c84d0622ce4d719170dab962
Nyíri Dániel <nyiridaniel3@gmail.com>
2026-04-21 22:19:44 +0200

    feat: setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:winamp_pro/core/configs/theme/app_theme.dart';
+import 'package:winamp_pro/firebase_options.dart';
 import 'package:winamp_pro/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:winamp_pro/presentation/splash/pages/splash.dart';
 
@@ -13,6 +15,10 @@ Future<void> main() async {
     storageDirectory: kIsWeb
       ? HydratedStorageDirectory.web
       : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
   );
   runApp(const MyApp());
 }
